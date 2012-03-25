9c169b23ffbac995c9bbeec7c8ca0eed05d90457
fbeppler <fdb@knewin.com>
2012-03-25 15:57:58 -0300

    Second commit!

--- first-test/src/MainFirstTest.java
+++ first-test/src/MainFirstTest.java
@@ -5,7 +5,7 @@ public class MainFirstTest {
 	 * @param args
 	 */
 	public static void main(final String[] args) {
-		System.out.println("First test...");
+		System.out.println("First test again...");
 	}
 
 }
