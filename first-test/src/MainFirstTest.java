3bd63aabc2dfa96dc439e5db80822c4bda1981cc
fbeppler <fdb@knewin.com>
2012-03-25 22:19:41 -0300

    Commit with saved repository.

--- first-test/src/MainFirstTest.java
+++ first-test/src/MainFirstTest.java
@@ -4,7 +4,7 @@ public class MainFirstTest {
 	 * @param args
 	 */
 	public static void main(final String[] args) {
-		System.out.println("First test again. Again? Yes!!! Fifth time!");
+		System.out.println("First test with saved repository!");
 	}
 
 }
