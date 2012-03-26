88f4a5089752988a9a97d7eb04803174803b1f7b
fabiano-beppler <fdb@knewin.com>
2012-03-25 22:21:20 -0300

    Update first-test/src/MainFirstTest.java

--- first-test/src/MainFirstTest.java
+++ first-test/src/MainFirstTest.java
@@ -4,7 +4,7 @@ public class MainFirstTest {
 	 * @param args
 	 */
 	public static void main(final String[] args) {
-		System.out.println("First test with saved repository!");
+		System.out.println("First test with saved repository! And now its test is about fetch!");
 	}
 
 }
